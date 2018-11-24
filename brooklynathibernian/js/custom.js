37f56773ce0ccd8231ba048c70031b45644466ae
Taylor Pechacek <taylortpechacek@gmail.com>
2018-11-24 16:50:41 +1100

    I think this fixea the loading the page before css problem

--- brooklynathibernian/js/custom.js
+++ brooklynathibernian/js/custom.js
@@ -1,105 +1,107 @@
-(function($) { "use strict";
+(function($) {
+  'use strict';
 
-    // Preload all the images in the page.
-    imagesLoaded(document.querySelectorAll('.content'), {background: true}, () => document.body.classList.remove('loading'));
-	
-	/*! Popover */
-	
-	$('a.ticket-link').webuiPopover({
-		url:'#ticket-wrap',
-		width: 300,
-		animation: 'pop',
-		arrow: true
-	});
+  // Preload all the images in the page.
+  imagesLoaded(
+    document.querySelectorAll('.content'),
+    { background: true },
+    () => document.body.classList.remove('loading')
+  );
 
-	
-	$(document).ready(function() {
+  /*! Popover */
 
-		/*! Nicescroll */
-		
-		$("#boxscroll-1").niceScroll("#boxscroll-in-1",{
-			cursorcolor:"#000",
-			cursoropacitymax:0.5,
-			cursorwidth: "6px",
-			cursorborder: "0px solid #fff",
-			dblclickzoom: false,
-			scrollspeed: 60,
-			mousescrollstep: 60,
-			autohidemode: false,
-			bouncescroll: false,
-			railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
-		});
-		$("#boxscroll-2").niceScroll("#boxscroll-in-2",{
-			cursorcolor:"#000",
-			cursoropacitymax:0.5,
-			cursorwidth: "6px",
-			cursorborder: "0px solid #fff",
-			dblclickzoom: false,
-			scrollspeed: 60,
-			mousescrollstep: 60,
-			autohidemode: false,
-			bouncescroll: false,			
-			railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
-		});
-		$("#boxscroll-3").niceScroll("#boxscroll-in-3",{
-			cursorcolor:"#000",
-			cursoropacitymax:0.5,
-			cursorwidth: "6px",
-			cursorborder: "0px solid #fff",
-			dblclickzoom: false,
-			scrollspeed: 60,
-			mousescrollstep: 60,
-			autohidemode: false,
-			bouncescroll: false,		
-			railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
-		});
-		$("#boxscroll-4").niceScroll("#boxscroll-in-4",{
-			cursorcolor:"#000",
-			cursoropacitymax:0.5,
-			cursorwidth: "6px",
-			cursorborder: "0px solid #fff",
-			dblclickzoom: false,
-			scrollspeed: 60,
-			mousescrollstep: 60,	
-			autohidemode: false,
-			bouncescroll: false,		
-			railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
-		});
-		$("#boxscroll-5").niceScroll("#boxscroll-in-5",{
-			cursorcolor:"#000",
-			cursoropacitymax:0.5,
-			cursorwidth: "6px",
-			cursorborder: "0px solid #fff",
-			dblclickzoom: false,
-			scrollspeed: 60,
-			mousescrollstep: 60,
-			autohidemode: false,
-			bouncescroll: false,			
-			railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
-		});
+  $('a.ticket-link').webuiPopover({
+    url: '#ticket-wrap',
+    width: 300,
+    animation: 'pop',
+    arrow: true
+  });
 
-		
-		//Countdown
-		
-        $('.countdown').downCount({
-            date: '07/27/2019 12:00:00'
-        }, function () {
-            alert('WOOT WOOT, done!');
-        });
+  $(document).ready(function() {
+    /*! Nicescroll */
 
-		
-		//Animated Icons
-		
-		var options = {
-		  duration: 150, 
-		  type: 'oneByOne',
-		  animTimingFunction: Vivus.EASE
-		};
+    $('#boxscroll-1').niceScroll('#boxscroll-in-1', {
+      cursorcolor: '#000',
+      cursoropacitymax: 0.5,
+      cursorwidth: '6px',
+      cursorborder: '0px solid #fff',
+      dblclickzoom: false,
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      autohidemode: false,
+      bouncescroll: false,
+      railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
+    });
+    $('#boxscroll-2').niceScroll('#boxscroll-in-2', {
+      cursorcolor: '#000',
+      cursoropacitymax: 0.5,
+      cursorwidth: '6px',
+      cursorborder: '0px solid #fff',
+      dblclickzoom: false,
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      autohidemode: false,
+      bouncescroll: false,
+      railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
+    });
+    $('#boxscroll-3').niceScroll('#boxscroll-in-3', {
+      cursorcolor: '#000',
+      cursoropacitymax: 0.5,
+      cursorwidth: '6px',
+      cursorborder: '0px solid #fff',
+      dblclickzoom: false,
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      autohidemode: false,
+      bouncescroll: false,
+      railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
+    });
+    $('#boxscroll-4').niceScroll('#boxscroll-in-4', {
+      cursorcolor: '#000',
+      cursoropacitymax: 0.5,
+      cursorwidth: '6px',
+      cursorborder: '0px solid #fff',
+      dblclickzoom: false,
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      autohidemode: false,
+      bouncescroll: false,
+      railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
+    });
+    $('#boxscroll-5').niceScroll('#boxscroll-in-5', {
+      cursorcolor: '#000',
+      cursoropacitymax: 0.5,
+      cursorwidth: '6px',
+      cursorborder: '0px solid #fff',
+      dblclickzoom: false,
+      scrollspeed: 60,
+      mousescrollstep: 60,
+      autohidemode: false,
+      bouncescroll: false,
+      railpadding: { top: 20, right: 5, left: 0, bottom: 20 }
+    });
 
-		var vivus = new Vivus('svg-ticket',	options, onComplete);
+    //Countdown
 
-		function onComplete() {}
+    $('.countdown').downCount(
+      {
+        date: '07/27/2019 12:00:00'
+      },
+      function() {
+        alert('WOOT WOOT, done!');
+      }
+    );
 
-	});	
-	
-})(jQuery); 
+    //Animated Icons
+
+    var options = {
+      duration: 150,
+      type: 'oneByOne',
+      animTimingFunction: Vivus.EASE
+    };
+
+    var vivus = new Vivus('svg-ticket', options, onComplete);
+
+    function onComplete() {}
+  });
+})(jQuery);
